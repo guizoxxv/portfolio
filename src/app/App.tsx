4fa262e61b813541337937d40621290d994efe05
guizo <guizoxxv@gmail.com>
2019-09-08 13:57:03 -0300

    Clean + Sass + Css Modules + Tailwind

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header/Header';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <span>App</span>
+    </div>
+  );
+}
+
+export default App;
