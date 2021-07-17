4fc6cccaba35e8776427b7f7ae5aaacce59ffd7d
guizoxxv <guizoxxv@gmail.com>
2021-07-17 13:44:25 -0300

    Updating dependencies

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4c304b9584669f701319dc7e35ecff1.js"
+  "/precache-manifest.b37db1d62cfbdd6d541fe66cf86eefed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
